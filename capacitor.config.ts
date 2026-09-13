a513b35d3a6088c07925e3e18ee91d9a85c8bace
vishyramos0026-eng <vishyramos0026@gmail.com>
2026-09-13 13:59:23 +0800

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.photogallery',
+  appName: 'Photo Gallery',
+  webDir: 'dist'
+};
+
+export default config;
